2a641ef773f63b78c874db54e223c9c6ed24773a
valeru19 <135430416+valeru19@users.noreply.github.com>
2024-11-12 15:09:49 +0500

    Add files via upload

---
+++ web_lab_2/js/script.js
@@ -0,0 +1,5 @@
+document.querySelector('.burger_menu').addEventListener('click', function() {
+    const menu = document.querySelector('.header__menu');
+    menu.classList.toggle('open'); 
+  });
+  
